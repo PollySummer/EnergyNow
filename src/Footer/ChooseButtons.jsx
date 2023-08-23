cb53f0338c49dccc1ebacf63ef3f47535224549a
PollySummer <polinaivanenko.cat@gmail.com>
2023-08-23 16:54:36 +0300

    new

--- src/Footer/ChooseButtons.jsx
+++ src/Footer/ChooseButtons.jsx
@@ -1,35 +1,61 @@
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 
-const first = 'first';
-const second = 'second';
-const third = 'third';
-const fourth = 'fourth';
 function ChooseButtons() {
     const [choose, setChoosen] = useState(false);
 
     return (
         <>
-            <Button
-                variant="primary"
-                onClick={() => setChoosen(first)}
-                active={choose === first}
-            >1</Button>{' '}
-            <Button
-                variant="primary"
-                onClick={() => setChoosen(second)}
-                active={choose === second}
-            >2</Button>{' '}
-            <Button
-                variant="primary"
-                onClick={() => setChoosen(third)}
-                active={choose === third}
-            >3</Button>{' '}
-            <Button
-                variant="primary"
-                onClick={() => setChoosen(fourth)}
-                active={choose === fourth}
-            >4</Button>{' '}
+            <div className=' d-flex justify-content-center'>
+                <Button
+                    className='me-2'
+                    variant="warning"
+                    onClick={() => setChoosen('first')}
+                    active={choose === 'first'}
+                >1</Button>{' '}
+                <Button
+                    className='me-2'
+                    variant="warning"
+                    onClick={() => setChoosen('second')}
+                    active={choose === 'second'}
+                >2</Button>{' '}
+                <Button
+                    className='me-2'
+                    variant="warning"
+                    onClick={() => setChoosen('third')}
+                    active={choose === 'third'}
+                >3</Button>{' '}
+                <Button
+                    className='me-2'
+                    variant="warning"
+                    onClick={() => setChoosen('fourth')}
+                    active={choose === 'fourth'}
+                >4</Button>{' '}
+                <Button
+                    className='me-2'
+                    variant="warning"
+                    onClick={() => setChoosen('fifth')}
+                    active={choose === 'fifth'}
+                >5</Button>{' '}
+                <Button
+                    className='me-2'
+                    variant="warning"
+                    onClick={() => setChoosen('sixth')}
+                    active={choose === 'sixth'}
+                >6</Button>{' '}
+                <Button
+                    className='me-2'
+                    variant="warning"
+                    onClick={() => setChoosen('seventh')}
+                    active={choose === 'seventh'}
+                >7</Button>{' '}
+                <Button
+                    className='me-2'
+                    variant="warning"
+                    onClick={() => setChoosen('eight')}
+                    active={choose === 'eight'}
+                >8</Button>{' '}
+            </div>
         </>
     );
 }
