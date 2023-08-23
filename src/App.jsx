89105a694d79999db515802b796f28c65e05412e
PollySummer <polinaivanenko.cat@gmail.com>
2023-08-23 12:52:58 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Navigation from './Navigation/Navigation';
 import Header from './Body/Header';
 import Chart from './Body/Chart';
-import ChooseDays from './Body/ChooseDays';
+
 
 function App() {
   return (
@@ -11,7 +11,6 @@ function App() {
       <Navigation />
       <Header />
       <Chart/>
-      <ChooseDays/>
     </Container>
   );
 }
