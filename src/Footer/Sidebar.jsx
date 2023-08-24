104aec5d2c92507810d57643bd295191d0ad9cd2
PollySummer <polinaivanenko.cat@gmail.com>
2023-08-24 20:19:39 +0300

    DateForm

--- src/Footer/Sidebar.jsx
+++ src/Footer/Sidebar.jsx
@@ -1,14 +1,14 @@
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import DateForm from './DateForm';
 
-function Sidebar({show, handleClose}) {
+function Sidebar({ show, handleClose }) {
     return (
         <Offcanvas show={show} onHide={handleClose}>
             <Offcanvas.Header closeButton>
-                <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                <Offcanvas.Title>Date Search</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-                Some text as placeholder. In real life you can have the elements you
-                have chosen. Like, text, images, lists, etc.
+                <DateForm />
             </Offcanvas.Body>
         </Offcanvas>
     );
