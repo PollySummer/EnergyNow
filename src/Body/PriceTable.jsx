865c833c1c73dd1815811e46366e05ce6a69fbf1
PollySummer <polinaivanenko.cat@gmail.com>
2023-09-02 21:14:20 +0300

    added table

--- src/Body/PriceTable.jsx
+++ src/Body/PriceTable.jsx
@@ -1,17 +1,57 @@
 import Table from 'react-bootstrap/Table';
 import moment from 'moment';
-import { NOW_TIMESTAMP,ELECTR,GAS } from './consts';
+import { NOW_TIMESTAMP, ELECTR, GAS } from './consts';
+import { useEffect, useState } from 'react';
 
-function PriceTable({ electricityPrice}) {
+function PriceTable({ electricityPrice, activeEnergy, gasPrice }) {
+    const [tableData, setTableData] = useState([]);
 
-    const data = electricityPrice?.ee.map((priceEE, index) => {
-        return {
-            ee: priceEE,
-            lv: electricityPrice?.lv[index],
-            fi: electricityPrice?.fi[index],
-            lt: electricityPrice?.lt[index]
+    // const data = electricityPrice?.ee.map((priceEE, index) => {
+    //     return {
+    //         ee: priceEE,
+    //         lv: electricityPrice?.lv[index],
+    //         fi: electricityPrice?.fi[index],
+    //         lt: electricityPrice?.lt[index]
+    //     }
+    // });
+    useEffect(() => {
+        if (!electricityPrice || !gasPrice) return;
+
+        const energy = {
+            [ELECTR]: {
+                data: electricityPrice,
+                format: 'HH',
+                main: 'ee'
+            },
+            [GAS]: {
+                data: gasPrice,
+                format: 'DD',
+                main: 'common'
+            },
         }
-    });
+        //energy[activeEnergy: choosing ELECTR or GAS.  
+        // data - to refer to elements is [ELECTR ] AND [GAS]
+        //[energy[activeEnergy].main] - choosing 'ee' or 'common' (main element).
+        //and [energy[activeEnergy].main] in [] because we need to draw out the element
+
+
+        //use [] to refer to elements by const
+
+        const mainData = energy[activeEnergy].data;
+        const main = energy[activeEnergy].main;
+
+        const data = mainData[main].map((_, index) => {
+            return {
+                ee: energy[activeEnergy].data.ee[index],
+                lv: energy[activeEnergy].data.lv[index],
+                fi: energy[activeEnergy].data.fi[index],
+                lt: energy[activeEnergy].data.lt[index],
+                date: mainData[main][index]
+            }
+        });
+
+        setTableData(data);
+    }, [electricityPrice, gasPrice, activeEnergy]);
 
     return (
         <Table striped bordered hover>
@@ -26,13 +66,11 @@ function PriceTable({ electricityPrice}) {
                 </tr>
             </thead>
             <tbody>
-                {data.map(({ ee, lt, lv, fi }, index) => {
-                    const rowDate = moment.unix(ee.timestamp).format('DD.MM.YYYY HH:mm:ss');
-                    const now = moment().startOf('hour').format('DD.MM.YYYY HH:mm:ss');
+                {tableData.map(({ ee, lt, lv, fi, date }, index) => {
                     return (
-                        <tr key={index} >
+                        <tr key={index} className={NOW_TIMESTAMP === ee?.timestamp ? 'now' : ''}>
                             <td>{index}</td>
-                            <td style={{ backgroundColor: rowDate === now ? 'red' : '' }}>{rowDate}</td>
+                            <td>{moment.unix(date.timestamp).format('DD.MM.YYYY HH:mm:ss')}</td>
                             <td>{ee?.price}</td>
                             <td>{fi?.price}</td>
                             <td>{lt?.price}</td>
