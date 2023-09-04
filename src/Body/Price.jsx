604ff77398c5f0afa0186bfdde029751bed9cc7c
PollySummer <polinaivanenko.cat@gmail.com>
2023-09-04 13:39:25 +0300

    back up

--- src/Body/Price.jsx
+++ src/Body/Price.jsx
@@ -1,55 +1,20 @@
 import { useEffect, useState } from "react";
-import { NOW_TIMESTAMP, ELECTR, GAS } from "./consts";
+import { NOW_TIMESTAMP} from "./consts";
 
 
-function Price({ electricityPrice, activeEnergy, latestGasPrice }) {
-    const [currentElectrPrice, setCurrentElecrPrice] = useState(0);
-    const [currentGasPrice, setCurrentGasPrice] = useState(0);
+function Price({ electricityPrice}) {
+    const [currentPrice, setCurrentPrice] = useState(0);
+
 
     useEffect(() => {
-        if (!electricityPrice || !latestGasPrice) return;
-
-        const energy = {
-            [ELECTR]: {
-                data: electricityPrice.ee,
-            },
-            [GAS]: {
-                data: latestGasPrice,
-            },
-        }
-
-        const mainData = energy[activeEnergy].data.map((data) =>
-        ({
-            ...data,
-        })
-        )
-        if (activeEnergy === ELECTR) {
-            setCurrentElecrPrice(mainData);
-
-        } else if (activeEnergy === GAS) {
-            setCurrentGasPrice(mainData);
-        }
-
-    }, [electricityPrice, latestGasPrice, activeEnergy]);
-
-
-    // useEffect(() => {
-    //     if (!electricityPrice) return;
-    //     const { price } = electricityPrice?.ee.find(item => item.timestamp === NOW_TIMESTAMP);
-    //     setCurrentPrice(price);
-    // }, [electricityPrice]);
+        if (!electricityPrice) return;
+        const { price } = electricityPrice?.ee.find(item => item.timestamp === NOW_TIMESTAMP);
+        setCurrentPrice(price);
+    }, [electricityPrice]);
 
     return (
         <>
-            {
-                (activeEnergy === ELECTR ? currentElectrPrice : currentGasPrice) &&
-                (activeEnergy === ELECTR ? currentElectrPrice : currentGasPrice).map((item) => (
-                    <h3>
-                        {activeEnergy === ELECTR && item.timestamp === NOW_TIMESTAMP ?
-                            item.price : activeEnergy === GAS && item.price}
-                    </h3>
-                ))
-            }
+            <h3>{currentPrice}</h3>
             <div>sents/kw</div>
         </>
     )
