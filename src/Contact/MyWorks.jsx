5531b00f15178dfee9a550e9edf75cbe1e87986b
PollySummer <polinaivanenko.cat@gmail.com>
2023-09-11 13:56:24 +0300

    final

--- src/Contact/MyWorks.jsx
+++ src/Contact/MyWorks.jsx
@@ -3,7 +3,7 @@ import Carousel from 'react-bootstrap/Carousel';
 function MyWorks() {
     return (
         <>
-        <h2 className='text-center mt-5 mb-3'>My works</h2>
+            <h2 className='text-center mt-5 mb-3'>My works</h2>
             <Carousel data-bs-theme="dark">
                 <Carousel.Item>
                     <img
@@ -11,10 +11,6 @@ function MyWorks() {
                         src="myWork1.png"
                         alt="First slide"
                     />
-                    <Carousel.Caption>
-                        <h5>First slide label</h5>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -22,10 +18,6 @@ function MyWorks() {
                         src="myWork2.png"
                         alt="Second slide"
                     />
-                    <Carousel.Caption>
-                        <h5>Second slide label</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -33,12 +25,6 @@ function MyWorks() {
                         src="myWork3.png"
                         alt="Third slide"
                     />
-                    <Carousel.Caption>
-                        <h5>Third slide label</h5>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
-                    </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
         </>
