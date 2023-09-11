ff9a3fa92ab5548c2e98121007bad9e26dcd9235
PollySummer <polinaivanenko.cat@gmail.com>
2023-09-11 16:16:54 +0300

    finally final

--- src/Contact/ContactFooter.jsx
+++ src/Contact/ContactFooter.jsx
@@ -1,8 +1,13 @@
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
+import Toast from 'react-bootstrap/Toast';
+import { useState } from 'react';
 
 function ContactFooter() {
+    const [show, setShow] = useState(false);
+
     return (
+
         <div className='d-flex flex-column align-items-center'>
             <h2 className='text-center mt-5 mb-3'>Contact Me</h2>
             <Form className='w-50 m-auto'>
@@ -17,7 +22,19 @@ function ContactFooter() {
                     <Form.Control as="textarea" rows={3} />
                 </Form.Group>
             </Form>
-            <Button className='aboutBtn mb-5' as="input" type="submit" value="Submit" />
+            <Button onClick={() => setShow(true)} className='aboutBtn mb-5' as="input" type="submit" value="Submit" />
+            <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
+                <Toast.Header>
+                    <img
+                        src="holder.js/20x20?text=%20"
+                        className="rounded me-2"
+                        alt=""
+                    />
+                    <strong className="me-auto">EnergyToday</strong>
+                    <small>Now</small>
+                </Toast.Header>
+                <Toast.Body>Thank you for contacting us. We will answer you very soon!</Toast.Body>
+            </Toast>
             <div className='text-center p-4'>
                 © 2023 All rights reserved
                 <a className='nav-link' href='/'>
