5957c320798f30b346bd03290c2d5b6796379f00
PollySummer <polinaivanenko.cat@gmail.com>
2023-09-14 11:30:21 +0300

    bugs fixed

--- src/Contact/AboutMe.jsx
+++ src/Contact/AboutMe.jsx
@@ -8,7 +8,7 @@ function AboutMe() {
         <div className='aboutMe w-75 m-auto mt-5'>
             <Image className='aboutMePhoto rounded' src={`${mainUrl}/mainPhoto2.JPG`} />
             <h2 className=' mt-5 mb-3'>About Me</h2>
-            <p className='lead'>I learn HTML5, CSS3 Node.js and React with GammaIntelligence courses. I already learnt such frameworks as Bootstrap and SCSS. I chose this field because I like combining of programming and design in one speciality.
+            <p >I learn HTML5, CSS3 Node.js and React with GammaIntelligence courses. I already learnt such frameworks as Bootstrap and SCSS. I chose this field because I like combining of programming and design in one speciality.
                 And also I’m 4th year student of Ukrainian Admiral Makarov National University of Shipbuilding, learning software development.
                 For this time I learnt basis of C, C++ and Java languages. I also had experience of working with IntelliJ IDEA, MySQL and JavaFX. I created one project using Java and MySQL. I have worked with Windows, MacOS and a little with Linux Mint operating systems.
                 P.S. I also good in understanding programming memes :)
--- src/Prices/PricePage.jsx
+++ src/Prices/PricePage.jsx
@@ -2,12 +2,17 @@ import { useEffect, useState } from "react";
 import { NOW_TIMESTAMP } from "../Body/consts";
 import { useSelector } from "react-redux";
 import { useLocation } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import { useNavigate } from 'react-router-dom';
+
 
 function PricePage() {
     const electricityPrice = useSelector((state) => state.electricityPrice);
     const gasCurrentPrice = useSelector((state) => state.gasCurrentPrice);
     const [currentPrice, setCurrentPrice] = useState(0);
+    const checked = true;
     const { pathname } = useLocation();
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (!electricityPrice) return;
@@ -18,16 +23,42 @@ function PricePage() {
     const electricityWithTax = currentPrice + (currentPrice / 100) * 20;
     const gasPriceWithTax = (gasCurrentPrice + (gasCurrentPrice / 100) * 20);
 
-
     return (
-        <>
+        <div className="d-flex flex-column align-items-center">
             <h2>{pathname === '/PricePage/kw' ? 'Electricity price with tax:' : 'Electricity normal price'}</h2>
-            <h2>{pathname === '/PricePage/kw' ? electricityWithTax : currentPrice}</h2>
-            <div>sents/kw</div>
+            <h2 className="mt-2">{pathname === '/PricePage/kw' ? electricityWithTax : currentPrice}</h2>
+            <div className="mb-5">sents/kw</div>
             <h2>{pathname === '/PricePage/kw' ? 'Gas price with tax:' : 'Gas normal price'}</h2>
             <h2>{pathname === '/PricePage/kw' ? gasPriceWithTax : gasCurrentPrice}</h2>
-            <div>m³</div>
-        </>
+            <div className="mb-5">m³</div>
+
+
+            <div className="mb-5">
+                <Form.Check
+                    inline
+                    label="Price now"
+                    defaultChecked={checked}
+                    name="group1"
+                    type="radio"
+                    id={`inline-1`}
+                    onChange={() => navigate('/PricePage')}
+                />
+                <Form.Check
+                    inline
+                    label="Price with taxes"
+                    name="group1"
+                    type="radio"
+                    id={`inline-2`}
+                    onChange={() => navigate('/PricePage/kw')}
+                />
+            </div>
+            <div className='text-center p-4 mt-5'>
+                © 2023 All rights reserved
+                <a className='nav-link' href='/'>
+                    ReactApp | EnergyToday
+                </a>
+            </div>
+        </div>
     )
 }
 
